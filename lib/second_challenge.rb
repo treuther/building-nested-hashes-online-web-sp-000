f57e9d8fbf5ee895cc6f0cd0a460a1787bc7bf44
Tracy Reuther <tracyreuther@gmail.com>
2019-06-10 09:42:55 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -9,10 +9,10 @@ def second_challenge
       },
     :capulet => {
       #your key/value pairs here!
-      :patriarch
-      :matriarch
-      :heroine
-      :heroine_friends
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
       }}
 
 
